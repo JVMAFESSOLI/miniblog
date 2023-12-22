cce0b433f13447fe5dfb97c212e79b11fc1057bd
Vitor <jose.mafessoli@orsegups.com.br>
2023-12-22 16:01:27 -0300

    implement rhf on login screen

--- src/domain/auth/Login/Login.tsx
+++ src/domain/auth/Login/Login.tsx
@@ -1,29 +1,17 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
 import { useAuthentication } from "hooks";
 
 import styles from "./Login.module.css";
 
 export const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const { register, handleSubmit } = useForm();
   const [error, setError] = useState("");
 
   const { login, error: authError, loading } = useAuthentication();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    setError("");
-
-    const user = {
-      email,
-      password,
-    };
-
-    const res = await login(user);
-
-    console.log(res);
+  const onSubmit = async (data) => {
+    await login(data);
   };
 
   useEffect(() => {
@@ -36,30 +24,30 @@ export const Login = () => {
     <div className={styles.login}>
       <h1>Entrar</h1>
       <p>Faça o login para poder utilizar o sistema</p>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label>
           <span>E-mail:</span>
           <input
             type="email"
-            name="email"
             required
+            {...register("email")}
             placeholder="e-mail do Usuário"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
           />
         </label>
         <label>
           <span>Senha:</span>
           <input
             type="password"
-            name="password"
             required
+            {...register("password")}
             placeholder="Insira sua Senha"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
           />
         </label>
-        {!loading && <button className="btn">Entrar</button>}
+        {!loading && (
+          <button className="btn" type="submit">
+            Entrar
+          </button>
+        )}
         {loading && (
           <button className="btn" disabled>
             Aguarde...
